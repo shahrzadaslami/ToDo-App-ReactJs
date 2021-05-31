dc9a69d8874d8a3a40d7bc34fa49412a0b960ac2
shahrzad <raha.aslami@gmail.com>
2021-05-31 10:33:37 +0430

    Header module

--- src/App.js
+++ src/App.js
@@ -7,38 +7,16 @@ import '../node_modules/@popperjs/core/dist/cjs/popper'
 import '../node_modules/bootstrap/dist/js/bootstrap';
 import Form  from './components/Form';
 import TodoList from './components/TodoList';
+import Hedaer from './components/Header';
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setToDo] = useState([]);
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-      <header>
+      {/*<header>
         <h1>ReactJS To-Do list App</h1>
-      </header>
+      </header>*/}
+      <Hedaer/>
       <Form todos ={todos} inputText={inputText} setToDo={setToDo} setInputText={setInputText}/>
       <TodoList setToDo={setToDo} todos ={todos}/>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+const Header = ({}) =>{
+    return(
+        <div className="header">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Planner</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>   
+            <img className="logo" src="/logo.png" alt="" />
+            <div className="text pt-2">
+                <h2>Get it right this time</h2>
+            </div>
+            <div className="boxes">
+                <a href="">
+                    <div className="box m-1">
+                    <img src="/list.png" alt="" />
+                    </div>
+                </a>
+                <a href="">
+                    <div className="box m-1">
+                    <img src="/note.png" alt="" />
+                    </div>
+                </a>
+                <a href="">
+                    <div className="box m-1">
+                    <img src="/timer.png" alt="" />
+                    </div>
+                </a>
+            </div>
+      </div>
+    );
+}
+
+export default Header;
